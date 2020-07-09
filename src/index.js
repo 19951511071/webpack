e13f15eaeeb4b3dd242ac217364c46c14ce12d5d
weiyang <1274755779@qq.com>
2020-07-09 23:56:51 +0800

    最后一次更改

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import x from './x.js'
-console.log('hi')
-console.log(x)
+import png from './assets/1.png'
+console.log(png)
+
+const app1 = document.querySelector("#app1")
+app1.innerHTML = `
+
+<img src = "${png}">
+
+`
+const button = document.createElement('button')
+button.textContent = "懒加载"
+
+button.onclick = () => {
+    const promise = import ('./lazy')
+    promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => {
+        console.log("懒加载失败")
+    })
+}
+app1.appendChild(button)
